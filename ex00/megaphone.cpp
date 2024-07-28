5e740756cdbb074d55db760719aecce779cef323
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-07-28 16:17:19 +0200

    Ex00 is done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 14:25:00 by juestrel          #+#    #+#             */
-/*   Updated: 2024/07/28 14:41:57 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/07/28 16:13:45 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,23 @@
 
 int main(int argc, char *argv[])
 {
-	(void) argv;
+	unsigned int i = 1;
+	
 	if (argc == 1)
+	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" <<std::endl;
+		return (0);
+	}
+	while (argv[i])
+	{
+		unsigned int j = 0;
+		while (argv[i][j] != '\0')
+		{
+			std::cout << (char)toupper(argv[i][j]);
+			j++;
+		}
+		i++;
+	}
+	std::cout<< std::endl;
 	return (0);
 }
