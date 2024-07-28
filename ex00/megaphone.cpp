5f5d8c192622cf093ba8d63914de7f91662c37b8
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-07-28 14:44:03 +0200

    We have a basic Makefile and cpp file

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/28 14:25:00 by juestrel          #+#    #+#             */
+/*   Updated: 2024/07/28 14:41:57 by juestrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" <<std::endl;
+	return (0);
+}
