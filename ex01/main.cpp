f95457e6a10ef63d6f3c0e7952bf7b33f62fba55
juestrel <128648875+juan-est145@users.noreply.github.com>
2024-09-02 18:01:14 +0200

    Solved closing of standard input issue in main.cpp file

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 17:08:43 by juestrel          #+#    #+#             */
-/*   Updated: 2024/07/28 20:35:14 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/09/02 17:59:16 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ int main(void)
 			phoneBook.add();
 		else if (cmd == "SEARCH")
 			phoneBook.search();
+		else if (std::cin.eof())
+			exitMessage();
 		else
 			std::cout << "Try again" << std::endl;
 	}
@@ -38,4 +40,10 @@ void greeting()
 	std::cout << "SEARCH: Display a specific contact\n";
 	std::cout << "EXIT: Quits the program and deletes all contacts\n"
 			  << std::endl;
+}
+
+void exitMessage()
+{
+	std::cout << "The standard input was closed, exiting program\n";
+	exit(0);
 }
--- ex01/main.hpp
+++ ex01/main.hpp
@@ -1,9 +1,10 @@
 #ifndef MAIN_HPP
-#define MAIN_HPP
-#include <iostream>
-#include <string>
-#include "PhoneBook.hpp"
-#include "Contact.hpp"
+# define MAIN_HPP
+# include "Contact.hpp"
+# include "PhoneBook.hpp"
+# include <iostream>
+# include <string>
 
-void greeting();
+void	greeting(void);
+void	exitMessage(void);
 #endif
